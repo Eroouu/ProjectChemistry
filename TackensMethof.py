aac81858fcbd8c55940f923c733c12cbf1c66da9
Шайхиев Эрик Радикович <shajhiev.erik-r@net.ugatu.su>
2024-01-06 22:36:13 +0500

    немного почистил код

--- TackensMethof.py
+++ TackensMethof.py
@@ -43,6 +43,34 @@ def FindManyValuesInVector(vector, n, e, proc_decriese, counte):
         array.append(FindSpecFunkValue(vector, n, e))
         e *= (1-proc_decriese )
     return array
+def Making_Covar_Matrix(time_series, k):
+    N = len(time_series)
+    series_mean = np.mean(time_series)
+    C = np.zeros((k, k))
+    for i in range(k):
+        ti = time_series[i:i + N - k + 1]
+        ti_mean = np.mean(ti)
+        C[i, i] = np.dot(ti - ti_mean, ti - ti_mean)
+        for j in range(i + 1, k):
+            tj = time_series[j:j + N - k + 1]
+            tj_mean = np.mean(tj)
+            C[i, j] = np.dot(ti - ti_mean, tj - tj_mean)
+            C[j, i] = C[i, j]
+    print(np.trace(C))
+    return C
+def Covar_Matrix_Inf(C, k):
+    plt.figure(figsize=(9, 7), dpi=80)
+    sns.heatmap(C, annot=True, fmt='g', xticklabels=[i for i in range(k)], yticklabels=[i for i in range(k)])
+    plt.title(f"Матрица ковариации для k = {k}", fontsize=18)
+    plt.show()
+    eigenvalues, eigenvectors = LA.eig(C)
+    print(f'собственные значения матрицы для k={k}')
+    df = pd.DataFrame(eigenvalues)
+    print(df)
+    x = np.array([i + 1 for i in range(k)])
+    plt.plot(x, np.log(eigenvalues))
+    plt.title(f"График значений собственных чисел для k = {k}", fontsize=14)
+    plt.show()
 def The_broken_cane_method(C, k):
     # реализация метода сломаной трости
     eigenvalues, eigenvectors = LA.eig(C)
@@ -58,30 +86,10 @@ def The_broken_cane_method(C, k):
     print('\n', df)
     return 0
 def MCM(time_series,time_array,  k):
-    N = len(time_series)
-    series_mean = np.mean(time_series)
-    el_count = len(time_series) - k + 1
-    data = np.array([time_series[i:i + k] for i in range(el_count)])
-    npQ = np.cov(data)
-    C = np.zeros((k, k))
-    for i in range(k):
-        ti = time_series[i:i + N - k + 1]
-        C[i,i] = np.dot(ti - series_mean, ti - series_mean)
-        for j in range(i+1,k):
-            tj = time_series[j:j + N - k + 1]
-            C[i, j] = np.dot(ti - series_mean, tj - series_mean)
-            C[j, i] = C[i, j]
-    plt.figure(figsize=(9, 7), dpi=80)
-    sns.heatmap(C, annot=True, fmt='g', xticklabels=[i for i in range(k)], yticklabels=[i for i in range(k)])
-    plt.title(f"Матрица ковариации для k = {k}", fontsize=18)
-    plt.show()
-    eigenvalues, eigenvectors = LA.eig(C)
-    print(f'собственные значения матрицы для k={k}')
-    df = pd.DataFrame(eigenvalues)
-    print(df)
-    x = np.array([i + 1 for i in range(k)])
-    plt.plot(x, np.log(eigenvalues))
-    plt.title(f"График значений собственных чисел для k = {k}", fontsize=14)
-    plt.show()
-    The_broken_cane_method(C, k)
+    data = np.array([time_series[i:i + len(time_series) - k] for i in range(k)])
+    npQ = np.cov(data, bias=True)
+    C = Making_Covar_Matrix(time_series, k)
+    #print(C, '\n', npQ)
+    #Covar_Matrix_Inf(C,k)
+    #The_broken_cane_method(C, k)
     return 0
