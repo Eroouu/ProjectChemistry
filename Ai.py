f59446faa5189287c2dd04222bcf26ecd8af8997
Марат <bitkulovmarat@gmail.com>
2024-01-27 19:41:17 +0500

    Проба sklearn. Версия_2

--- Ai.py
+++ Ai.py
@@ -2,8 +2,9 @@ import openpyxl
 from sklearn import decomposition
 import pandas as pd
 def AI_Gen():
-    keep_cols = [i for i in range(1,4)]
-    df = pd.read_excel('Мед_сталь_микротрубка_15В.xlsx', sheet_name='Стальная_труб',skiprows=lambda x: x in [0, 1], usecols = keep_cols)
+    keep_cols = [i for i in range(1,67)]
+    df = pd.read_excel('Медь_толстая_проволока№1_15В.xlsx', sheet_name='Cu1',skiprows=lambda x: x in [0, 1], usecols = keep_cols)
+    df.columns = df.columns.astype(str)
     pca = decomposition.PCA()
     pca.fit(df)
     pca.transform(df)
